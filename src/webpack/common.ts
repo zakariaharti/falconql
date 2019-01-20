589170360dede458e026c8a1c9fb7752b52630ee
zakariaharti <zakariya.developer@gmail.com>
2019-01-20 22:09:15 +0100

    common

---
+++ src/webpack/common.ts
@@ -0,0 +1,16 @@
+/*
+ * webpack common configuration
+ */
+
+// ----------------------------------------------------------------
+// IMPORTS
+
+/** NODE */
+import * as path from "path";
+
+/** NPM */
+import * as webpack from "webpack";
+
+// ----------------------------------------------------------------
+
+const root = path.dirname(__dirname);
