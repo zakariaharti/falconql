716caee7f2dbed8be8b08cac13493e0a8c821b60
zakariaharti <zakariya.developer@gmail.com>
2019-01-21 14:00:32 +0100

    common webpack

--- src/webpack/common.ts
+++ src/webpack/common.ts
@@ -9,8 +9,73 @@
 import * as path from "path";
 
 /** NPM */
-import * as webpack from "webpack";
+import {Configuration} from "webpack";
 
 // ----------------------------------------------------------------
 
-const root = path.dirname(__dirname);
+const root = path.resolve(__dirname, "..", "..");
+const configFileName = path.resolve(root,'tsconfig.json');
+
+const isProdMode = process.env.NODE_ENV === 'production';
+
+// common config
+const config: Configuration = {
+  mode: isProdMode ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              plugins: [
+                "@babel/plugin-syntax-dynamic-import",
+                [
+                  "@babel/plugin-proposal-decorators",
+                  {
+                    legacy: true
+                  }
+                ],
+                [
+                  "@babel/plugin-proposal-class-properties",
+                  {
+                    loose: true
+                  }
+                ],
+                "react-hot-loader/babel",
+                [
+                  "babel-plugin-styled-components",
+                  {
+                    displayName: !isProdMode
+                  }
+                ]
+              ]
+            }
+          },
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              configFileName
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+      publicPath: "/"
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(root, "src")
+    },
+    extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+    modules: [path.resolve(root, "node_modules")]
+  }
+}
+
+// exports
+export default config;
