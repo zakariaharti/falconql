a73d1022eacb124107c1ce2c4801e2d172693059
zakariaharti <zakariya.developer@gmail.com>
2019-01-24 20:02:35 +0100

    apollo types

---
+++ src/client/types/apollo/index.ts
@@ -0,0 +1,12 @@
+// apollo common types
+// -----------------------------------------------------------------------------
+
+/* STATE */
+export interface IState {
+  count: number;
+}
+
+// 'Root', which contains the 'State' key
+export interface IRoot {
+  state: IState;
+}
