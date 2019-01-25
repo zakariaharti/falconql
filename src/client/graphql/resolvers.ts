265a67fd506155394c5cc4937bc27a36f2770e0e
zakariaharti <zakariya.developer@gmail.com>
2019-01-25 19:54:36 +0100

    resolvers

---
+++ src/client/graphql/resolvers.ts
@@ -0,0 +1,14 @@
+// resolvers
+// ----------------------------------------------------------------------------
+
+// IMPORTS
+
+/** NPM */
+import _ from 'lodash';
+
+/** LOCAL */
+import countResolver from './count/resolver';
+
+// ----------------------------------------------------------------------------
+
+export default _.merge(countResolver);
