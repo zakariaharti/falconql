94c93b11b56d9e50e97d5d8321b674c0dc5dc8db
zakariaharti <zakariya.developer@gmail.com>
2019-01-28 19:04:56 +0100

    count

---
+++ src/client/app/Count/Count.tsx
@@ -0,0 +1,20 @@
+// Exapmle Count feature
+
+// -----------------------------------------------------------------------------
+// IMPORTS
+
+/** NPM */
+import * as React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+/** LOCAL */
+import COUNT_QUERY from '../../graphql/count/queries/getCount';
+
+// -----------------------------------------------------------------------------
+
+const Count: React.SFC<{}> = () => {
+  return <p></p>
+};
+
+export default Count;
