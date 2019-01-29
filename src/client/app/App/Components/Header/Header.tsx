94dc9e461da578c094bf6cf022893067eb1274e6
zakariaharti <zakariya.developer@gmail.com>
2019-01-29 20:13:13 +0100

    header

--- src/client/app/App/Components/Header/Header.tsx
+++ src/client/app/App/Components/Header/Header.tsx
@@ -6,9 +6,29 @@
 /** NPM */
 import * as React from 'react';
 
+/** LOCAL */
+import styled from '@/client/helpers/styledComponents';
+
 // -----------------------------------------------------------------------------
 
+const StyledHeader = styled.div`
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledNavbar = styled.nav`
+  width: 100%;
+`;
+
 const Header: React.SFC<{}> = () => (
-  <div>
-  </div>
-)
+  <StyledHeader>
+    <h5>FalconQl Project</h5>
+    <StyledNavbar>
+      <a href="#">link 1</a>
+      <a href="#">link 2</a>
+      <a href="#">link 3</a>
+    </StyledNavbar>
+  </StyledHeader>
+);
+
+export default Header;
