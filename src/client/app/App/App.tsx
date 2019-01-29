b8f41827978a6e9ade031063ea941168fb7e9b32
zakariaharti <zakariya.developer@gmail.com>
2019-01-29 21:03:37 +0100

    app

--- src/client/app/App/App.tsx
+++ src/client/app/App/App.tsx
@@ -5,6 +5,44 @@
 
 /** NPM */
 import * as React from 'react';
-import helmet from 'react-helmet';
+import Helmet from 'react-helmet';
+import { hot }  from 'react-hot-loader';
+import { Switch }  from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { createGlobalStyle }  from 'styled-components';
 
 /** LOCAL */
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import { routes } from '../routes';
+
+// -----------------------------------------------------------------------------
+
+// global styles
+const StyledGlobal = createGlobalStyle`
+  :root {
+    box-sizing: border-box;
+  }
+
+  html,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+  }
+`;
+
+const App: React.SFC<{}> = () => (
+  <div>
+    <StyledGlobal />
+    <Helmet>
+      <title>FalconQl - Starter Project</title>
+    </Helmet>
+    <Header />
+    <Switch>
+      {renderRoutes(routes)}
+    </Switch>
+    <Footer />
+  </div>
+);
+
+export default hot(module)(App);
