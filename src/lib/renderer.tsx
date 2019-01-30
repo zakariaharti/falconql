09a5111447d5ef3ebe097cbef6e29770b23a718a
zakariaharti <zakariya.developer@gmail.com>
2019-01-30 20:03:05 +0100

    renderer

---
+++ src/lib/renderer.tsx
@@ -0,0 +1,58 @@
+// Server-side HTML render
+
+// Component to render the full HTML response in React
+
+// ----------------------------------------------------------------------------
+// IMPORTS
+
+/* NPM */
+import * as React from "react";
+import { HelmetData } from "react-helmet";
+
+// ----------------------------------------------------------------------------
+
+// Types
+
+export interface IHtmlProps {
+  css?: string;
+  helmet: HelmetData;
+  html: string;
+  scripts: string[];
+  styles?: Array<React.ReactElement<{}>>;
+  window?: {
+    [key: string]: object;
+  };
+}
+
+export default class Html extends React.PureComponent<IHtmlProps> {
+  public render() {
+    const { css, helmet, html, scripts, styles } = this.props;
+    return (
+      <html
+        lang="en"
+        {...helmet.htmlAttributes.toString()}
+      >
+        <head>
+          {helmet.title.toComponent()}
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="Content-Language" content="en" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {helmet.meta.toComponent()}
+          {helmet.style.toComponent()}
+          {helmet.link.toComponent()}
+          {css && <link rel="stylesheet" href={css} />}
+          {styles}
+          {helmet.script.toComponent()}
+          {helmet.noscript.toComponent()}
+        </head>
+        <body {...helmet.bodyAttributes.toComponent()}>
+          <div id="root" dangerouslySetInnerHTML={{ __html: html }} />
+        </body>
+        {scripts.map(script => (
+          <script key={script} src={script} />
+        ))}
+      </html>
+    );
+  }
+}
