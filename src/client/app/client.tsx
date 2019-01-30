f2b3fa69818f10a21e678f130030cd48a60e4872
zakariaharti <zakariya.developer@gmail.com>
2019-01-30 19:56:35 +0100

    client

---
+++ src/client/app/client.tsx
@@ -0,0 +1,31 @@
+// Client entry point
+
+// ----------------------------------------------------------------------------
+// IMPORTS
+
+/* NPM */
+
+import * as React from "react";
+import { ApolloProvider } from "react-apollo";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+/* Local */
+import App from './App/App';
+import { createClient } from '@/client/graphql';
+
+// ----------------------------------------------------------------------------
+
+// Create Apollo client
+const client = createClient();
+
+// render components
+const root = document.getElementById("root")!;
+ReactDOM.hydrate(
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
+  root
+);
