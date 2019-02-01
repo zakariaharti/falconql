8bf81221fa9e9d097aa4648a8b22d7134c262295
zakariaharti <zakariya.developer@gmail.com>
2019-02-01 13:57:53 +0100

    up

--- src/scripts/static.ts
+++ src/scripts/static.ts
@@ -5,15 +5,33 @@
 
 /* NPM */
 import * as chalk from "chalk";
+const historyFallback = require("koa2-history-api-fallback");
 
 /* Local */
-import { buildInfo, runner } from "./runner";
+// import staticMiddleware from "../lib/staticMiddleware";
+import { build, common, app, staticCompiler, devServer } from "./app";
 
 // ----------------------------------------------------------------------------
 
-buildInfo.spinner.info(chalk.default.bgBlue("Static mode"));
+common.spinner.info(chalk.default.bgBlue("Static mode"));
 
-(async () => {
-  await runner().build(true);
-  buildInfo.spinner.succeed("Finished building");
+void (async () => {
+  // Production?
+  if (common.isProduction) {
+    common.spinner.info("Building production files...");
+    await build(true /* build in static mode */);
+    common.spinner.succeed("Finished building");
+    return;
+  }
+
+  // Development...
+  common.spinner.info("Building development server...");
+
+  app.listen({ port: common.port, host: "localhost" }, async () => {
+    // Fallback to /index.html on 404 routes, for client-side SPAs
+    app.use(historyFallback());
+
+    // Build the static dev server
+    await devServer(app, staticCompiler);
+  });
 })();
