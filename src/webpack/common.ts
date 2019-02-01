df52d08bbab75ee62d6b4fa036d25fea81733f88
zakariaharti <zakariya.developer@gmail.com>
2019-02-01 13:46:58 +0100

    update

--- src/webpack/common.ts
+++ src/webpack/common.ts
@@ -55,7 +55,7 @@ const config: Configuration = {
             }
           },
           {
-            loader: 'awesome-typescript-loader',
+            loader: 'ts-loader',
             options: {
               configFileName
             }
