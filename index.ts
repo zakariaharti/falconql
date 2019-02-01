b6375dbc306f6d60eed7670998f20224bd89d8a9
zakariaharti <zakariya.developer@gmail.com>
2019-02-01 13:37:32 +0100

    entry project

---
+++ index.ts
@@ -0,0 +1,28 @@
+import * as path from "path";
+import * as fs from "fs";
+
+// Load env vars, for the `GRAPHQL` endpoint and anything else we need
+require("dotenv").config();
+
+// Catch CTRL/CMD+C interrupts cleanly
+process.on("SIGINT", () => {
+  process.exit(0);
+});
+
+// Check that we have a specified Webpack runner
+if (!process.env.RUNNER) {
+  console.error("No Webpack runner specified");
+  process.exit(1);
+}
+
+// Path to runner
+const script = path.resolve("./src/runner", `${process.env.RUNNER!}.ts`);
+
+// Check that the runner exists
+if (!fs.existsSync(script)) {
+  console.error(`Runner doesn't exist: ${script}`);
+  process.exit(1);
+}
+
+// Start the script
+require(script);
