a18f598fb222f77c55bfcdfc33c9b52e15bc3ea9
zakariaharti <zakariya.developer@gmail.com>
2019-02-02 21:53:51 +0100

    update server

--- server/server.tsx
+++ server/server.tsx
@@ -2,24 +2,75 @@
 
 // ----------------------------------------------------------------------------
 // IMPORTS
-     
+
 /* NPM */
 
 import * as React from "react";
 import { Context } from "koa";
 import { ApolloProvider, getDataFromTree } from "react-apollo";
 import * as ReactDOMServer from "react-dom/server";
-import Helmet from "react-helmet";
 import { StaticRouter } from "react-router";
+import * as Koa from "koa";
+import * as koaCors from "@koa/cors";
+import * as KoaRouter from "koa-router";
+import * as webpack from "webpack";
+import * as KoaWebpack from "koa-webpack";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
 /** LOCAL */
 
-import App from "@/client/app/App/App";
-import { createClient } from "@/client/graphql";
-import Output from "@/lib/output";
-import Renderer from "@/lib/renderer";
+import App from "../client/modules/App/App";
+import { createClient } from "../client/graphql";
+import { renderer } from "../lib/renderer";
 
 // ----------------------------------------------------------------------------
+const isProdMode = process.env.NODE_ENV === 'production' || false;
+
+// Router
+const router = new KoaRouter()
+  .get("/ping", async ctx => {
+    ctx.body = "pong";
+  })
+  .get("/favicon.ico", async ctx => {
+    ctx.status = 204;
+  });
+
+// Koa instance
+export const app = new Koa()
+
+  // CORS
+  .use(koaCors())
+
+  // Error catcher
+  .use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (e) {
+      console.log("Error:", e);
+      ctx.status = 500;
+      ctx.body = "There was an error. Please try again later.";
+    }
+  });
+
+app.use(router.allowedMethods()).use(router.routes());
+
+// in development mode
+if (!isProdMode) {
+   const webpack = require('webpack');
+   const config = require('../webpack/client.js');
+   const compiler = webpack(config);
+
+   app.use(async () => {
+     await KoaWebpack({
+       compiler,
+       devMiddleware: {
+         logLevel: "info",
+         publicPath: "/",
+         stats: false
+       }
+     });
+   });
+}
 
 // Types
 export interface IRouterContext {
@@ -27,74 +78,65 @@ export interface IRouterContext {
   url?: string;
 }
 
-// Everything from this point will be Webpack'd and dumped in `dist/server.js`
-// and then loaded into an active Koa server
-export default function(output: Output) {
-  // Create Koa middleware to handle React requests
-  return async (ctx: Context) => {
-    // Create a new Apollo client
-    const client = createClient();
+app.use(async (ctx: Context) => {
+  // Create a new Apollo client
+  const client = createClient();
 
-    // Create a fresh 'context' for React Router
-    const routerContext: IRouterContext = {};
+  // styled-components ssr support
+ const sheet = new ServerStyleSheet();
 
-    // Render our components - passing down MobX state, a GraphQL client,
-    // and a router for rendering based on our route config
-    const components = (
+  // Create a fresh 'context' for React Router
+  const routerContext: IRouterContext = {};
+
+  // Render our components - passing down MobX state, a GraphQL client,
+  // and a router for rendering based on our route config
+  const components = (
+      <StyleSheetManager sheet={sheet}>
         <ApolloProvider client={client}>
           <StaticRouter location={ctx.request.url} context={routerContext}>
             <App />
           </StaticRouter>
         </ApolloProvider>
-    );
+      </StyleSheetManager>
+  );
 
-    // Await GraphQL data coming from the API server
-    await getDataFromTree(components);
+  // Await GraphQL data coming from the API server
+  await getDataFromTree(components);
 
-    // Handle 301/302 redirects
-    if ([301, 302].includes(routerContext.status!)) {
-      // 301 = permanent redirect, 302 = temporary
-      ctx.status = routerContext.status!;
+  // Handle 301/302 redirects
+  if ([301, 302].includes(routerContext.status!)) {
+    // 301 = permanent redirect, 302 = temporary
+    ctx.status = routerContext.status!;
 
-      // Issue the new `Location:` header
-      ctx.redirect(routerContext.url!);
+    // Issue the new `Location:` header
+    ctx.redirect(routerContext.url!);
 
-      // Return early -- no need to set a response body
-      return;
-    }
+    // Return early -- no need to set a response body
+    return;
+  }
 
-    // Handle 404 `Not Found`
-    if (routerContext.status === 404) {
-      // By default, just set the status code to 404. You can
-      // modify this section to do things like log errors to a
-      // third-party, or redirect users to a dedicated 404 page
+  // Handle 404 `Not Found`
+  if (routerContext.status === 404) {
+    // By default, just set the status code to 404. You can
+    // modify this section to do things like log errors to a
+    // third-party, or redirect users to a dedicated 404 page
 
-      ctx.status = 404;
-      ctx.body = "Not found";
+    ctx.status = 404;
+    ctx.body = "Not found";
 
-      return;
-    }
+    return;
+  }
 
-    // Create response HTML
-    const html = ReactDOMServer.renderToString(components);
+  // Create response HTML
+  const html = ReactDOMServer.renderToString(components);
 
-    // Create the React render, and inject the `<head>` section
-    // courtesy of React Helmet.
-    const reactRender = ReactDOMServer.renderToString(
-      <Renderer
-        css={output.client.main("css")!}
-        helmet={Helmet.renderStatic()}
-        html={html}
-        scripts={output.client.scripts()}
-        window={{
-          __APOLLO_STATE__: client.extract(), // <-- GraphQL store
-        }}
-      />
-    );
+  // Create the React render, and inject the `<head>` section
+  // courtesy of React Helmet.
+  const finalView = renderer(html, {}, client.extract(), sheet);
 
-    // Set the return type to `text/html`, and dump the response back to
-    // the client
-    ctx.type = "text/html";
-    ctx.body = `<!DOCTYPE html>${reactRender}`;
-  };
-}
+  // Set the return type to `text/html`, and dump the response back to
+  // the client
+  ctx.type = "text/html";
+  ctx.status = 200;
+  ctx.body = `${finalView}`;
+});
